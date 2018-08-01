387b44c8c0da13f568ff59a1b58aaae0c33f9677
Weeravit Sirita <weeravit.sirita@gmail.com>
2018-08-01 23:39:33 +0700

    move main activity to modules package

---
+++ app/src/main/java/xyz/devnote/orderfoodonline/modules/MainActivity.kt
@@ -0,0 +1,13 @@
+package xyz.devnote.orderfoodonline.modules
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import xyz.devnote.orderfoodonline.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
